e7fd24aee08b14ad30fa20df9cab635b35ba9500
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2023-08-17 02:03:14 +0600

    optimized the logic for the deposit and withdraw calculation

--- account.js
+++ account.js
@@ -11,38 +11,29 @@ const balanceShow = document.getElementById('show-balance');
 // deposit calculation logic
 depositBtn.addEventListener('click', function () {
     // Deposit showing code
-    const currentValue = depositShow.innerText;
-    const sz = currentValue.length;
-    let currentMoney = currentValue.slice(1, sz);
-    const depositInput = deposit.value;
-    currentMoney = parseInt(currentMoney) + parseInt(depositInput);
-    depositShow.innerText = '$' + currentMoney;
+    let currentMoney = depositShow.innerText;
+    let depositInput = deposit.value;
+    currentMoney = parseFloat(currentMoney) + parseFloat(depositInput);
+    depositShow.innerText = currentMoney;
 
     // Balance showing code
-    const currentBalance = balanceShow.innerText;
-    console.log(currentBalance);
-    const len = currentBalance.length;
-    let currentBalanceMoney = currentBalance.slice(1, len);
-    currentBalanceMoney = parseInt(currentBalanceMoney) + parseInt(depositInput);
-    balanceShow.innerText = '$' + currentBalanceMoney;
+    let currentBalanceMoney = balanceShow.innerText;
+    currentBalanceMoney = parseFloat(currentBalanceMoney) + parseFloat(depositInput);
+    balanceShow.innerText = currentBalanceMoney;
     deposit.value = '';
 })
 
 // withdraw calculation logic
 withdrawBtn.addEventListener('click', function () {
     // Withdraw showing code
-    const currentValue = withdrawShow.innerText;
-    const sz = currentValue.length;
-    let currentMoney = currentValue.slice(1, sz);
-    const withdrawInput = withdraw.value;
-    currentMoney = parseInt(currentMoney) + parseInt(withdrawInput);
-    withdrawShow.innerText = '$' + currentMoney;
+    let currentMoney = withdrawShow.innerText;
+    let withdrawInput = withdraw.value;
+    currentMoney = parseFloat(currentMoney) + parseFloat(withdrawInput);
+    withdrawShow.innerText = currentMoney;
 
     // Balance showing code
-    const currentBalance = balanceShow.innerText;
-    const len = currentBalance.length;
-    let currentBalanceMoney = currentBalance.slice(1, len);
-    currentBalanceMoney = parseInt(currentBalanceMoney) - parseInt(withdrawInput);
-    balanceShow.innerText = '$' + currentBalanceMoney;
+    let currentBalanceMoney = balanceShow.innerText;
+    currentBalanceMoney = parseFloat(currentBalanceMoney) - parseFloat(withdrawInput);
+    balanceShow.innerText = currentBalanceMoney;
     withdraw.value = '';
 })
